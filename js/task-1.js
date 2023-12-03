5f79b4d4a346fd9ba1655f62925857a3d547855e
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2023-12-03 18:52:15 +0200

    upd

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,30 @@
-function slugify(title) {
-  
-  const slugifiedTitle = title.toLowerCase().replace(/\s+/g, '-');
-  
-  return slugifiedTitle;
-  
+function isEnoughCapacity(products, containerSize) {
+
+
+let values = Object.values(products);
+let totalSum = 0;
+for (const item of values) {
+  totalSum +=item;
+}
+
+if (totalSum <= containerSize) {
+  return 'True';} else {return 'False'};
+
   
 }
 
-console.log(slugify("Arrays for begginers"));
-console.log(slugify("English for developer"));
-console.log(slugify("Ten secrets of JavaScript"));
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+);
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+);
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+);
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+);
